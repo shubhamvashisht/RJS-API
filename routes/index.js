1312225e0e2235de18474f27bc8a982401b0cbfd
shubhamvashisht <vashisht.s21295@gmail.com>
2018-02-06 21:49:45 +0530

    ready to role

---
+++ routes/index.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var path = require('path')
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/state',function(req, res){
+	var directories = fs.readdirSync('./public/assets/sections')
+	var result = [];
+	for(let i=0;i<directories.length;i++){
+		result.push({
+			state : directories[i]
+		})
+	}
+	var jsonRes = JSON.stringify(result)
+	console.log(jsonRes)
+	res.send(jsonRes)
+})
+
+router.get('/state/:state/',function(req,res){
+	var filesList=[]
+	fs.readdir('./public/assets/sections/'+req.params.state,function(err,files){
+		if (err){
+			console.log('no files exist')
+		}
+		else{
+			for(let i=0;i<files.length;i++){
+				filesList.push({
+					id : i,
+					link : path.resolve('./public/assets/sections/'+req.params.state+"/"+files[i]),
+					name : files[i]
+				})
+			}
+			var jsonRes = JSON.stringify(filesList)
+			res.send(jsonRes)
+		}
+	})
+})
+module.exports = router;
